a1cc5b3394b774fb9f3dca824ee5b28a3589890d
mrobin <mrobin@companeo.com>
2015-07-27 12:01:06 +0200

    remove things about measure date

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 /*globals module, require */
 
 module.exports = {
-    getGestationalAge: function (crl, measureDate) {
+    getGestationalAge: function (crl) {
         'use strict';
 
-        var moment = require('moment'),
-            numberedCrl = Number(crl),
-            mmtMeasureDate,
+        var numberedCrl = Number(crl),
             gestationalAgeInDays;
 
         if (0 < numberedCrl) {
-            //mmtMeasureDate = moment(measureDate);
             gestationalAgeInDays = ((Math.round(1000 * (8.052 * Math.pow(numberedCrl, 0.5) + 23.73))) / 1000);
         } else {
             throw 'Invalid crown-rump length.';
