a86f5bdf24cbaf448658f9fa5f03cca2042a33df
mrobin <mrobin@companeo.com>
2015-07-27 12:00:45 +0200

    calculate gestational age + corresponding units tests + jest-cli with coverage

---
+++ tests/getGestationalAge.specs.js
@@ -0,0 +1,47 @@
+/*globals jest, require, expect, describe, it */
+jest.autoMockOff();
+
+var testedThing = require('../index');
+
+describe('getGestationalAge', function () {
+    it('to be described correctly', function () {
+        expect(typeof testedThing.getGestationalAge).toBe('function');
+    });
+
+    it('should return correct numeric gestational age', function () {
+        expect(testedThing.getGestationalAge(24)).toEqual(63.177);
+        expect(testedThing.getGestationalAge(43)).toEqual(76.53);
+    });
+
+    it('should return correct string gestational age', function () {
+        expect(testedThing.getGestationalAge('24')).toEqual(63.177);
+        expect(testedThing.getGestationalAge('43')).toEqual(76.53);
+    });
+
+    it('should throw for stupid param', function () {
+        try {
+            expect(testedThing.getGestationalAge());
+            expect(true).toBe(false);
+        } catch (eX) {
+            expect(true).toBe(true);
+        }
+        try {
+            expect(testedThing.getGestationalAge(null));
+            expect(true).toBe(false);
+        } catch (eX) {
+            expect(true).toBe(true);
+        }
+        try {
+            expect(testedThing.getGestationalAge(''));
+            expect(true).toBe(false);
+        } catch (eX) {
+            expect(true).toBe(true);
+        }
+        try {
+            expect(testedThing.getGestationalAge('abcd'));
+            expect(true).toBe(false);
+        } catch (eX) {
+            expect(true).toBe(true);
+        }
+    });
+});
